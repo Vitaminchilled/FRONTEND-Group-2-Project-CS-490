ad7d6b69dd40b6b38ff682f5b5a514fd11cfa60a
Ben <Benporreca04@gmail.com>
2025-10-21 18:59:32 -0400

    Customer Info and Nav Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,69 @@
 import { NavLink } from "react-router-dom";
+import { useUser } from "../context/UserContext";
 import './NavBar.css'
 
 function NavBar() {
+  const {user, setUser} = useUser();
   return (
     <nav className='NavBar'>
       <ul className="NavLeft">
-        {/* NavLinks have a built in function to see what the active link is */}
-        {/* End determines if it will partially match a link, or if it must be exact */}
-        <li><NavLink  to="/" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Home</NavLink ></li>
-        <li><NavLink  to="/search" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Search</NavLink ></li>
+        {/* LEFT: None, Customer */}
+        {(user.type === 'none' || user.type === 'customer') && (
+          <>
+            {/* NavLinks have a built in function to see what the active link is */}
+            {/* End determines if it will partially match a link, or if it must be exact */}
+            <li><NavLink  to="/" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Home</NavLink ></li>
+            <li><NavLink  to="/search" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Search</NavLink ></li>
+          </>
+        )}
+        {/* LEFT: Owner */}
+        {(user.type === 'owner') && (
+          <>
+            <li><NavLink  to="/" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Home</NavLink ></li>
+            <li><NavLink  to="/services" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Services</NavLink ></li>
+            <li><NavLink  to="/products" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Products</NavLink ></li>
+            <li><NavLink  to="/gallery" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Gallery</NavLink ></li>
+          </>
+        )}
+        {/* LEFT: Admin */}
+        {(user.type === 'admin') && (
+          <>
+            <li><NavLink  to="/" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Salons</NavLink ></li>
+            <li><NavLink  to="/users" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Users</NavLink ></li>
+            <li><NavLink  to="/verify" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Verify</NavLink ></li>
+          </>
+        )}
       </ul>
+        
       <ul className="NavRight">
-        <li><NavLink  to="/login" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Login</NavLink ></li>
-        <li><NavLink  to="/signup" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Sign Up</NavLink ></li>
-        <li><NavLink  to="/register-salon" className="Register">Register Salon</NavLink ></li>
+        {/* RIGHT: None */}
+        {(user.type === 'none') && (
+          <>
+            <li><NavLink  to="/login" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Login</NavLink ></li>
+            <li><NavLink  to="/signup" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Sign Up</NavLink ></li>
+            <li><NavLink  to="/register-salon" className="Square">Register Salon</NavLink ></li>
+          </>
+        )}
+        {/* RIGHT: Customer */}
+        {(user.type === 'customer') && (
+          <>
+            <li><NavLink  to="/logout" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Log Out</NavLink ></li>
+            <li><NavLink  to="/register-salon" className="Square">{user.name}</NavLink ></li>
+          </>
+        )}
+        {/* RIGHT: Owner */}
+        {(user.type === 'owner') && (
+          <>
+            <li><NavLink  to="/logout" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Log Out</NavLink ></li>
+            <li><NavLink  to="/register-salon" className="Square">{user.name}</NavLink ></li>
+          </>
+        )}
+        {/* RIGHT: Admin */}
+        {(user.type === 'admin') && (
+          <>
+            <li><NavLink  to="/logout" end={false} className={({ isActive }) => isActive ? "Selected" : ""}>Log Out</NavLink ></li>
+          </>
+        )}
       </ul>
     </nav>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { UserProvider } from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      {/* Main Application built in App */}
-      <App />
+      {/* Creates a Single User that all children can access */}
+      <UserProvider>
+        {/* Main Application built in App */}
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>,
 )
