9cfe979c4345c2a13e6211175e632e1934056d00
LizethLino <lwl@njit.edu>
2025-10-22 16:31:17 -0400

    Dropdown navbar also dont look at search

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBar from './components/NavBar.jsx'
+import Home from './pages/Home.jsx'
+import SearchSalons from './pages/SearchSalons.jsx'
+import Account from './pages/AccountDetails.jsx'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        {/* Controls Links */}
+        <NavBar />
+
+        {/* Controls Routing to Pages */}
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchSalons />} />
+            <Route path="/account" element={<Account/>}/>
+            {/*
+            Routes to include:
+              /account is account details and should depend on the account type
+              /appointment-rewards is customer "My Appointments & Rewards"
+
+              /business-calendar
+              /manage-service
+              /manage-employee
+              /manage-product
+              /manage-gallery
+              /services
+              /products
+              /gallery
+
+              /login
+              /signup
+              /register-salon
+
+              / for admin which is tentatively the salons list to view analytics
+              /users for admin list of users and some analytics
+              /verify for admin list of businesses to verify
+
+              consider adding a path to view a specific salon dashboard as a customer
+              /salons/:salon_id
+            */}
+        </Routes>
+    </div>
   )
 }
 
