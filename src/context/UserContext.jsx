5df58ba9c34fcee157baf8789a1f3175f92f1451
Ben <Benporreca04@gmail.com>
2025-11-11 18:05:40 -0500

    added mock user_id

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,8 +4,9 @@ const UserContext = createContext();
 
 export function UserProvider({ children }){
     const [user, setUser] = useState({
-        type: 'customer', //none, customer, owner, admin
-        name: 'John Doe'
+        type: 'admin', //none, customer, owner, admin
+        name: 'John Doe',
+        user_id: 5
     });
     return (
         <UserContext.Provider value={{user, setUser}}>
