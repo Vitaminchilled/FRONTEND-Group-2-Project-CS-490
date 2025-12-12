4f25771ff99d6836f48aadbd03d2cf35a7e05990
Vitaminchilled <110579804+Vitaminchilled@users.noreply.github.com>
2025-12-12 00:14:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://backend-group-2-project-cs-490.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
